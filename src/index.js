e6252a23a6598bbcedf76db3c1cded0de09f5d76
DaniCis <danicisneros2@hotmail.com>
2022-10-16 02:03:52 -0500

    create react app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const taskRoutes = require('./routes/tasks.routes');
 
 
 const app = express();
 
 app.use(morgan('dev'))
+app.use(cors())
 app.use(express.json())
 
 app.use(taskRoutes)
