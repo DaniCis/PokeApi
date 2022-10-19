3227caec1c47193caf640b223d1792039ac2cebd
DaniCis <danicisneros2@hotmail.com>
2022-10-19 18:10:32 -0500

    buscar un pokemon

--- front/src/components/List.js
+++ front/src/components/List.js
@@ -13,7 +13,8 @@ export default function List() {
 
   const navigate = useNavigate()
   const [pokemons,setPokemons] = useState([])
-   
+  const [searchName, setSearchName] = useState('')
+
   useEffect(()=>{
     async function getPokemons(){
       try{
@@ -27,28 +28,47 @@ export default function List() {
     getPokemons()
   },[])
 
-  const selectPokemon = (id) =>{
+  async function search(name){
+    try{
+      const response = await pokemonApi.get(`/pokemon/search/${name}`)
+      setPokemons(response.data)
+    }catch(e){
+      console.log(e.message)
+    }
+  }
+  
+  const selectPokemon = (id) => {
     navigate(`/pokemon/${id}`)
   }
+
+  const searchPokemon = () => {
+    search(searchName)
+  }
+
+  
+
 
   return (
     <div>
       <Stack direction="horizontal" gap={3}>
-        <Form.Control className="me-auto" placeholder="..." />
-        <Button variant="secondary">Search</Button>
+        <Form.Control className="me-auto" placeholder="Search for pokemon" onChange={(e) => setSearchName(e.target.value)} />
+        <Button variant="secondary" onClick={() => {searchPokemon()}}>Search</Button>
         <div className="vr" />
         <Button variant="outline-danger">Clear</Button>
       </Stack>
+
       <Container className="card-container">
         <Row xs={1} md={2} lg={4} className="g-4">
           {pokemons.map((pokemon, key) => (
             <Col key={pokemon.id_pokemon}>
               <Card className="card">
                 <Card.Img className='card-img' variant="top" src={pokemon.imagen_pokemon} />
-                <Card.Body className="card-body">
+                <Card.Body>
                   <Card.Title className="card-title">{pokemon.nombre_pokemon}</Card.Title>
                   <Card.Text className="card-text">{pokemon.descripcion_pokemon}</Card.Text>
-                  <Button className='card-btn' onClick={()=>selectPokemon(pokemon.id_pokemon)} >Ver Detalles</Button>
+                  <div className="card-body-btn">
+                    <Button className='card-btn' onClick={()=>selectPokemon(pokemon.id_pokemon)} >Ver Detalles</Button>
+                  </div>
                 </Card.Body>
               </Card>
             </Col>
