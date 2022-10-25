2fb0987e4bbbffa275b2cb7cfb618ad627ec378d
DaniCis <danicisneros2@hotmail.com>
2022-10-25 00:06:38 -0500

    añadir nuevo .env

--- back/src/index.js
+++ back/src/index.js
@@ -2,9 +2,6 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const pokeRoutes = require('./routes/pokemon-routes')
-const path = require('path')
-
-
 const swaggerUI = require('swagger-ui-express')
 const swaggerJsDocs = require('swagger-jsdoc')
 const options = require('./docs')
@@ -16,13 +13,11 @@ app.use(cors())
 app.use(express.json())
 app.use("/docs", swaggerUI.serve ,swaggerUI.setup(swaggerJsDocs(options)))
 
-
 app.use(pokeRoutes)
 app.use((err,req,res,next) =>{
     return res.json({
         message:err.message
     })
 })
-
 
 app.listen(4000)
